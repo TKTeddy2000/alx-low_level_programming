cb59beb569a90c666307d41c4183c447edc2e0c8
tkteddy2000 <tkteddy2000@yahoo.com>
2023-02-24 10:27:43 -0800

    summation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @i: first input
+ * @j: second input
+ *
+ * Return: sum of i & j
+ */
+
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
