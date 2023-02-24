59a2670f7a5ad5000a4d6c29aeb027ecb9c8cec4
tkteddy2000 <tkteddy2000@yahoo.com>
2023-02-23 21:47:25 -0800

    Alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,15 @@
 #include "main.h"
 
 /**
- * print_alphabet Use _putchar function to print
+ * main - Entry point
  *
- * alphabet a to z
+ * Description: print_alphabet
+ * from a - z
+ *
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+
+void print_alphabet(void)/* function prints a - z */
 {
 	int i;
 
